a90c8b5a6ce395ca2d7bff3046ac3a33c1acfc3d
AchantaTharun <achantatharun0@gmail.com>
2023-12-09 20:25:05 -0500

    [BUG FIX] Create session

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -38,8 +38,19 @@ export const createSession = async (req, res) => {
       return res.status(400).json({ errors });
     }
 
+    if (!sessionSlots || sessionSlots.length === 0) {
+      return res.status(400).json({
+        errors: ['Please enter at least one session slot'],
+      });
+    }
+
     const currentDate = new Date();
 
+    if (new Date(endDate) < new Date(startDate)) {
+      return res.status(400).json({
+        errors: ['End date cannot be after Start Date'],
+      });
+    }
     if (new Date(startDate) < currentDate || new Date(endDate) < currentDate) {
       return res.status(400).json({
         errors: [
