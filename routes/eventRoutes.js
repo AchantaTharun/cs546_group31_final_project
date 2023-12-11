9f2cc8f5c39e776e646568bc5e7d1c11fb7e4c5d
rudranshnagar <rudranshnagar@hotmail.com>
2023-12-11 01:19:54 -0500

    defined routes for event APIs

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,9 +1,39 @@
 import express from "express";
 
 import { Router } from "express";
+import * as eventController from "../controllers/eventController.js";
+import * as authController from "../controllers/authController.js"; // If you're using this
 
 const router = Router();
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Event route" });
-});
+
+router
+  .get("/", eventController.getAllEvents);
+
+router
+  .post("/create", authController.protectRoute, eventController.createEvent);
+
+router
+  .route("/:eventId")
+  .get(authController.protectRoute, eventController.getEventById)
+  .patch(authController.protectRoute, eventController.updateEvent)
+  .delete(authController.protectRoute, eventController.deleteEvent);
+
+router
+  .post('/:eventId/comments',
+    authController.protectRoute, eventController.addComment);
+
+router
+  .delete('/:eventId/comments/:commentId',
+    authController.protectRoute, eventController.deleteComment);
+
+router
+  .patch('/:eventId/comments/:commentId',
+    authController.protectRoute, eventController.updateComment);
+
+router
+  .post('/:eventId/attendees',
+    authController.protectRoute, eventController.addAttendee)
+  .delete('/:eventId/attendees/:attendeeId',
+    authController.protectRoute, eventController.removeAttendee);
+  
 export default router;
