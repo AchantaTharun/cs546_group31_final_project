8a13157aaccb05774f91960d64d3d96603305608
rudranshnagar <rudranshnagar@hotmail.com>
2023-12-15 12:53:19 -0500

    Added missing code - eventRoutes

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,11 +1,39 @@
+import express from "express";
+
 import { Router } from "express";
-import Event from "../models/eventModel.js";
+import * as eventController from "../controllers/eventController.js";
+import * as authController from "../controllers/authController.js";
+
 const router = Router();
-router.get("/", async (req, res) => {
-  const events = await Event.find({});
-  return res.status(200).json({
-    status: "success",
-    data: events,
-  });
-});
+
+router
+  .get("/", eventController.getAllEvents);
+
+router
+  .post("/create", authController.protectRoute, eventController.createEvent);
+
+router
+  .route("/:eventId")
+  .get(authController.protectRoute, eventController.getEventById)
+  .patch(authController.protectRoute, eventController.updateEvent)
+  .delete(authController.protectRoute, eventController.deleteEvent);
+
+router
+  .post('/:eventId/comments',
+    authController.protectRoute, eventController.addComment);
+
+router
+  .delete('/:eventId/comments/:commentId',
+    authController.protectRoute, eventController.deleteComment);
+
+router
+  .patch('/:eventId/comments/:commentId',
+    authController.protectRoute, eventController.updateComment);
+
+router
+  .post('/:eventId/:attendeeId',
+    authController.protectRoute, eventController.addAttendee)
+  .delete('/:eventId/:attendeeId/remove',
+    authController.protectRoute, eventController.removeAttendee);
+  
 export default router;
